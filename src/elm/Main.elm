694389fc31480af8828233c629880c0dbb82703d
davejmac <david.mcnamara@outlook.com>
2017-02-09 15:04:37 -0500

    output list of inputted values

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,4 +1,63 @@
-import Html exposing (text)
+import Html exposing (Html, button, div, text, input, h2, p)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import String exposing (join)
+
+
 
 main =
-  text "Hello, World!"
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { valuesList : String
+  , newValue : String
+  }
+
+
+model : Model
+model =
+  { valuesList = ""
+  , newValue = ""
+  }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Add
+    | NewValue String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Add ->
+      { model | valuesList = model.valuesList ++ ", " ++ model.newValue }
+    NewValue text_input ->
+      { model | newValue = text_input }
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type_ "number", placeholder "Some Number", onInput NewValue ] []
+    , button [ onClick Add ] [ text "Add" ]
+    , div []
+      [ h2 [] [ (text "Numbers") ]
+      , p [] [ (text model.valuesList) ]
+      ]
+    ]
